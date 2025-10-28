fd25b6368fc1a7b8244a49088b041c8e42867578
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2025-10-28 09:32:49 -0600

    added failing tests for hasCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,27 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    private CustomList mockCityList() {
+        CustomList cityList = new CustomList();
+        cityList.addCity(mockCity());
+        return cityList;
+    }
 
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testHasCity() {
+        CustomList cityList = mockCityList();
+        City city = mockCity();
+        assertTrue(cityList.hasCity(city));
+    }
+
+    @Test
+    void testNotHasCity() {
+        CustomList cityList = mockCityList();
+        City city = new City("Calgary", "Alberta");
+        assertFalse(cityList.hasCity(city));
+    }
 }
